78c4006a4a00ae257854755f49e07f3400c50926
Kyle Long <uilwen@gmail.com>
2015-12-16 20:07:14 -0600

    adding error response mapping
    I am not sold on the way this is done.  I will reconsider it once
    I run into more errors.

--- pyshelf/app.py
+++ pyshelf/app.py
@@ -1,16 +1,16 @@
 import flask
 from pyshelf.routes.artifact import artifact
+import pyshelf.response_map as response_map
 
 app = flask.Flask(__name__)
 app.register_blueprint(artifact, url_prefix="/artifact")
 
 @app.after_request
 def format_response(response):
-    data = response.get_data()
     if response.status_code == 404:
-        # TODO : Map a json response
-        pass
+        response = response_map.create_404()
 
+    data = response.get_data()
     data += "\n"
     response.set_data(data)
     return response
---
+++ pyshelf/response_map.py
@@ -0,0 +1,27 @@
+from pyshelf.json_response import JsonResponse
+from pyshelf.error_code import ErrorCode
+
+
+def vnd_error(error):
+    """
+        Creates vnd.error type error responses
+        https://github.com/blongden/vnd.error
+    """
+    body = {
+       "code": error["code"],
+       "message": error["message"]
+    }
+    status_code = error.get("status_code", 500)
+    response = JsonResponse()
+    response.status_code = status_code
+    response.data = body
+    return response
+
+def create_404():
+    error = {
+        "code": ErrorCode.RESOURCE_NOT_FOUND,
+        "message": "Not found",
+        "status_code": 404
+    }
+
+    return vnd_error(error)
