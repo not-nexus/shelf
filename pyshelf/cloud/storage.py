9e9a53105e2a27278dbe3f578e15f557367123c9
Kyle Long <uilwen@gmail.com>
2015-12-17 17:43:34 -0600

    adding StreamIterator
    For large file download

--- pyshelf/cloud/storage.py
+++ pyshelf/cloud/storage.py
@@ -1,6 +1,7 @@
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 import os
+from pyshelf.cloud.stream_iterator import StreamIterator
 
 class Storage(object):
     def __init__(self, accessKey, secretKey):
@@ -10,12 +11,33 @@ class Storage(object):
         self.articlePath = os.path.expanduser('~') + '/tmp/'
 
     def get_artifact(self, bucketName, artifactName):
-        bucket = self.conn.get_bucket(bucketName)
-        key = bucket.get_key(artifactName)
+        key = self._get_key(bucketName, artifactName)
         dir = self.articlePath + artifactName
         fp = open(dir, 'w+')
         key.get_file(fp)
 
+    def get_artifact_stream(self, bucketName, artifactName):
+        """
+            Returns an object that can be used as a generator.
+            This should be used when streaming large files
+            directly to the client.
+
+            http://technology.jana.com/2015/03/12/using-flask-and-boto-to-create-a-proxy-to-s3/
+
+            Args:
+                bucketName(basestring): The name of the cloud storage bucket
+                    (S3 right now) that we want to connect to
+                artifactName(basestring): Full path to an object that you wish
+                    to download.
+
+            Returns:
+                pyshelf.cloud.stream_iterator.StreamIterator: A object that 
+                    implements a generator interface so can be passed 
+                    directly into a response so long as the framework supports it.
+        """
+        key = self._get_key(bucketName, artifactName)
+        stream = StreamIterator(key)
+        return stream
 
     def upload_artifact(self, bucketName, artifactName, fp):
         bucket = self.conn.get_bucket(bucketName)           
@@ -26,3 +48,8 @@ class Storage(object):
         bucket = self.conn.get_bucket(bucketName)
         key = bucket.get_key(artifactName)
         key.delete()
+
+    def _get_key(self, bucketName, artifactName):
+        bucket = self.conn.get_bucket(bucketName)
+        key = bucket.get_key(artifactName)
+        return key
---
+++ pyshelf/cloud/stream_iterator.py
@@ -0,0 +1,11 @@
+class StreamIterator(object):
+    """
+        Just a wrapper for the boto.s3.key.Key generator
+        functionality.  This is to provide a clean interface
+        so that replacing boto in the future would be easier.
+    """
+    def __init__(self, key):
+        self.key = key
+
+    def next(self):
+        return self.key.next()
