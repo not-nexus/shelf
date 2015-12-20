eb3a72a4318eb93db7d1662fd631968045362a1c
Kyle Long <uilwen@gmail.com>
2015-12-20 17:44:45 -0600

    adding a couple more codes

--- pyshelf/error_code.py
+++ pyshelf/error_code.py
@@ -1,3 +1,5 @@
 class ErrorCode(object):
     RESOURCE_NOT_FOUND = "resource_not_found"
     INTERNAL_SERVER_ERROR = "internal_server_error"
+    DUPLICATE_ARTIFACT = "duplicate_artifac"
+    INVALID_ARTIFACT_NAME = "invalid_artifact_name"
