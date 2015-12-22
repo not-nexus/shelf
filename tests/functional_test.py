e3e82d911e0ef1c54358a277404b3556b596768c
Kyle Long <uilwen@gmail.com>
2015-12-22 17:17:44 -0600

    actually using path

--- tests/functional_test.py
+++ tests/functional_test.py
@@ -24,7 +24,7 @@ class FunctionalTest(pyproctor.TestBase):
         CloudFactoryStub.reset()
 
     def get_artifact_path(self, path, status_code=200, body=None):
-        artifact = self.test_client.get("/artifact/test", headers={"Authorization": "supersecuretoken"})
+        artifact = self.test_client.get(path, headers={"Authorization": "supersecuretoken"})
         self.assert_response(status_code, artifact, body)
 
     def assert_response(self, status_code, response, body=None):
@@ -47,14 +47,14 @@ class FunctionalTest(pyproctor.TestBase):
 
     def upload_artifact(self, path, status_code=201, body=None):
         response = self.test_client.post(
-            "/artifact/test",
+            path,
             data={'file': (StringIO('file contents'), 'test.txt')},
             headers={"Authorization": "supersecuretoken", "Content-Type": "multipart/form-data"})
 
         self.assert_response(status_code, response, body)
 
     def test_artifact_get_path(self):
-        self.get_artifact_path("test", 200, "hello world")
+        self.get_artifact_path("/artifact/test", 200, "hello world")
 
     def test_artifact_upload(self):
-        self.upload_artifact("test", 201)
+        self.upload_artifact("/artifact/test", 201)
