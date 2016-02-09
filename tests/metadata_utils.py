2ef3ca4d04ff20828f0cfb7e9a8acc790dd26429
Andy Gertjejansen <AndyGertjejansen@outlook.com>
2016-02-08 22:27:14 -0600

    Split out metadata utils from permission utils.

---
+++ tests/metadata_utils.py
@@ -0,0 +1,54 @@
+import json
+
+
+MD5 = "5eb63bbbe01eeed093cb22bb8f5acdc3"
+
+
+def get_meta_item():
+    return {
+            "tag2": {
+                    "name": "tag2",
+                    "value": "test",
+                    "immutable": False
+                    }
+           }
+
+
+def get_meta():
+    return {
+            "tag": {
+                    "name": "tag",
+                    "value": "test",
+                    "immutable": False
+                    },
+            "tag1": {
+                    "name": "tag1",
+                    "value": "test1",
+                    "immutable": True
+                    },
+            "md5Hash": {
+                    "name": "md5Hash",
+                    "value": MD5,
+                    "immutable": True
+                    }
+            }
+
+
+def get_md5Hash():
+    return get_meta()["md5Hash"]
+
+
+def send_meta():
+    d = get_meta()
+    d.pop("md5Hash")
+    return json.dumps(d)
+
+
+def send_meta_item():
+    return json.dumps(get_meta_item())
+
+
+def send_meta_changed():
+    d = get_meta()
+    d["tag1"]["value"] = "changed value"
+    return json.dumps(dic)
--- tests/permission_utils.py
+++ tests/permission_utils.py
@@ -1,8 +1,5 @@
-import json
-
-
 VALID_KEY = "190a64931e6e49ccb9917c7f32a29d19"
-MD5 = "5eb63bbbe01eeed093cb22bb8f5acdc3"
+
 
 def get_permissions_all():
     return """
@@ -37,59 +34,6 @@ def get_permissions_func_test():
           - '/dir/dir2/'
           - '/dir/dir2/dir3/*'""".format(VALID_KEY)
 
-def get_meta_item():
-    return {
-            "tag2":{
-                    "name": "tag2", 
-                    "value": "test", 
-                    "immutable": False 
-                }
-           }
-
-def get_meta():
-    return { 
-            "tag":{
-                    "name": "tag", 
-                    "value": "test", 
-                    "immutable": False 
-                },
-            "tag1":{
-                    "name": "tag1", 
-                    "value": "test1", 
-                    "immutable": True 
-                }
-            }
-
-
-def get_meta_body():
-    return [ 
-                {
-                    "name": "md5Hash", 
-                    "value": MD5, 
-                    "immutable": True 
-                },
-                {
-                    "name": "tag", 
-                    "value": "test", 
-                    "immutable": False 
-                },
-                {
-                    "name": "tag1", 
-                    "value": "test1", 
-                    "immutable": True 
-                }
-           ]
-
-def send_meta():
-    return json.dumps(get_meta())
-
-def send_meta_item():
-    return json.dumps(get_meta_item())
-
-def send_meta_changed():
-    dic = get_meta()
-    dic["tag1"]["value"] = "changed value"
-    return json.dumps(dic)
 
 def auth_header(is_valid=True):
     if is_valid:
