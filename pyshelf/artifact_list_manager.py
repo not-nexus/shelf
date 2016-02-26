5d940c1e611f09c3fa2a690996c98f2226b9dc3f
Andy Gertjejansen <AndyGertjejansen@outlook.com>
2016-02-25 19:24:03 -0600

    Adding self link.

--- pyshelf/artifact_list_manager.py
+++ pyshelf/artifact_list_manager.py
@@ -20,25 +20,30 @@ class ArtifactListManager(object):
             if path[-1] == "/":
                 self.container.logger.debug("Artifact with path {} is a directory.".format(path))
                 child_list = storage.get_directory_contents(path)
-                links = []
-                for child in child_list:
-                    title = child.name
-                    url = "/artifact/" + title
-                    rel = "child"
-                    if child.name == path:
-                        rel = "self"
-                    links.append(self._format_link(url=url, rel=rel, title=title))
                 response = Response()
-                response.headers["Link"] = ",".join(links)
+                response.headers["Link"] = self._format_link_list(list(child_list), path)
                 response.status_code = 204
 
             else:
                 stream = storage.get_artifact(path)
                 response = Response(stream)
+                response.headers["Link"] = self._format_link(stream.key)
                 response.headers["Content-Type"] = stream.headers["content-type"]
 
         return response
 
-    def _format_link(self, **kwargs):
-        link = "<{url}>; rel={rel}; title={title}".format(**kwargs)
-        return link
+    def _format_link(self, artifact, child=False):
+        title = artifact.name
+        url = "/artifact/" + title
+        rel = "self"
+        if child:
+            rel = "child"
+        return "{0}; rel={1}; title={2}".format(url, rel, title)
+
+    def _format_link_list(self, artifact_list, parent_path):
+        link_list = []
+        for artifact in artifact_list:
+            child = parent_path != artifact.name
+            link_list.append(self._format_link(artifact, child))
+
+        return ", ".join(link_list)
