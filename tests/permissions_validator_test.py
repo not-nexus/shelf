8eb2cd6243f6c9a22934b3096f5fbbc2af8024fd
quantumew <AndyGertjejansen@outlook.com>
2016-03-10 22:35:09 +0000

    Add test for permissions validator issue I found.

--- tests/permissions_validator_test.py
+++ tests/permissions_validator_test.py
@@ -1,5 +1,6 @@
 from unit_test_base import UnitTestBase
 from pyshelf.permissions_validator import PermissionsValidator
+from pyshelf.cloud.cloud_exceptions import ArtifactNotFoundError
 from mock import MagicMock
 import permission_utils as utils
 
@@ -39,3 +40,13 @@ class PermissionsUnitTest(UnitTestBase):
     def test_allowed_read_no_access(self):
         self.mock_dependencies(utils.get_permissions_func_test(), "/artifact/dir/test", "GET", utils.auth_header())
         self.assertFalse(self.validator.allowed())
+
+    def test_invalid_key(self):
+        """ This is testing fake Authorization header issues """
+        self.container.request.path = "/blah"
+        self.container.request.method = "GET"
+        self.container.request.headers = {"Authorization": "DOES NOT EXIST"}
+        self.storage.get_artifact_as_string = MagicMock()
+        self.storage.get_artifact_as_string.side_effect = ArtifactNotFoundError("DOES NOT EXIST")
+        permissions = PermissionsValidator(self.container)
+        self.assertFalse(permissions.allowed())
