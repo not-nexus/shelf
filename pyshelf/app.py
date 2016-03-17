b5068d644ffde56f302e9aee5b77e837a1d3e181
quantumew <AndyGertjejansen@outlook.com>
2016-03-17 16:22:54 -0500

    Add some logging to generic error handler.

--- pyshelf/app.py
+++ pyshelf/app.py
@@ -1,15 +1,23 @@
 import flask
 from pyshelf.routes.artifact import artifact
 import pyshelf.response_map as response_map
+import logging
 
 app = flask.Flask(__name__)
 app.register_blueprint(artifact)
 
+
 @app.errorhandler(Exception)
 def generic_exception_handler(error):
+    """
+        Prevents Exceptions flying all around the place.
+    """
+    logger = logging.getLogger(__name__)
+    logger.debug("Exception was caught by the generic exception handler, {0} - {1}".format(type(error), error))
     if not error.message:
         error.message = "Internal Server Error"
     return response_map.create_500(msg=error.message)
+
 
 @app.after_request
 def format_response(response):
