1c569f2a2dfb5c22f6b810ba2ed6d99ccc4267c2
quantumew <AndyGertjejansen@outlook.com>
2016-03-21 21:47:16 -0500

    Add search.parser and some doc blocks.

--- pyshelf/search/manager.py
+++ pyshelf/search/manager.py
@@ -1,13 +1,18 @@
-
-
 class Manager(object):
     def __init__(self, container):
         self.container = container
 
     def search(self, criteria, key_list=None):
+        """
+            Builds ElasticSearch query.
+
+            Args:
+                criteria(dict): Criteria to use to initiate search.
+                key_list(list): List of keys to receive back from a search.
+
+            Returns:
+                list(dict): each dict represents a search "hit" with the keys specified in key_list.
+        """
+        # Does an empty list represent a request for all keys??
         if not key_list:
             key_list = []
-        # searches elasticsearch
-        #
-        # returns a list of dicts that only have the
-        # keys specified in key list
--- pyshelf/search/portal.py
+++ pyshelf/search/portal.py
@@ -1,9 +1,18 @@
 from pyshelf.search.type import Type as SearchType
+from pyshelf.search.metadata import Metadata
 
 
 class Portal(object):
     def __init__(self, container):
         self.container = container
+        self.search_manager = self.container.search.manager
 
-    def search(self, search_criteria):
-        pass
+    def search(self, criteria):
+        """
+            Searches ElasticSearch metadata documents.
+
+            Args:
+                criteria(dict): Criteria to use to initiate search.
+        """
+        results = self.search_manager.search(criteria)
+        # Here is where I would use the refactored artifact list manager to store links on context
