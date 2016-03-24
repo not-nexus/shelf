04e70d156e847c90c66a19f8bb8595ecfc434df0
Kyle Long <uilwen@gmail.com>
2016-03-24 17:42:25 -0500

    adding some load functionality
    Completely untested.  Modeled after the current metadata_mapper in
    the cloud namespace.  This will eventually replace it

--- pyshelf/metadata/manager.py
+++ pyshelf/metadata/manager.py
@@ -1,3 +1,6 @@
+import yaml
+
+
 class Manager(object):
     def __init__(self, container):
         self.container = container
@@ -5,6 +8,22 @@ class Manager(object):
         # self.update_manager = self.container.search.update_manager
         self.identity = self.container.resource_identity
         self.mapper = self.container.mapper
+        self._metadata = None
+
+    @property
+    def metadata(self):
+        if not self._metadata:
+            self._metadata = self.load()
+
+        return self._metadata
 
     def load(self):
-        pass
+        with self.container.create_cloud_storage as storage:
+            metadata_id = self.identity.cloud_metadata
+            raw_meta = storage.get_artifact_as_string(metadata_id)
+            meta = yaml.load(raw_meta)
+
+            if not meta:
+                meta = {}
+
+        return meta
