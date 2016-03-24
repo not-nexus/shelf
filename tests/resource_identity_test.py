a37c09e352c2ce5440ba9113d6284387feaf5af7
Kyle Long <uilwen@gmail.com>
2016-03-24 15:07:12 -0500

    missing test case

--- tests/resource_identity_test.py
+++ tests/resource_identity_test.py
@@ -21,3 +21,8 @@ class ResourceIdentityTest(pyproctor.TestBase):
 
     def test_search(self):
         self.assertEqual("abc0068b8b6cdd87550a4fc0cb5b7dc538c51c9ca50d5e5f9f4f2f08fc80e24e", self.create().search)
+
+    def test_multiple_separators_and_no_leading_slash(self):
+        identity = ResourceIdentity("lol-test//artifact///blah1//blah2")
+        self.assertEqual("/blah1/blah2", identity.path)
+        self.assertEqual("lol-test", identity.bucket_name)
