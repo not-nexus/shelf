5f1362db10f62ac66c2edf00686ef7bd7dca1cbe
Kyle Long <uilwen@gmail.com>
2016-04-08 11:17:20 -0500

    needed a way to get that metadata

--- pyshelf/bucket_update/artifact_metadata_updater.py
+++ pyshelf/bucket_update/artifact_metadata_updater.py
@@ -7,17 +7,17 @@ class ArtifactMetadataUpdater(object):
         """
         self.container = container
         self.identity = identity
+        self._metadata = None
+
+    @property
+    def metadata(self):
+        return self._metadata
 
     def run(self):
-        metadata = self._load_metadata()
-        import pprint; pprint.pprint(metadata)
-
-    def _load_metadata(self):
         portal = self.container.cloud_portal
         initializer = self.container.initializer
         metadata = portal.load(self.identity.cloud_metadata)
         if initializer.needs_update(metadata):
             metadata = initializer.update(self.identity, metadata)
             portal.update(self.identity.cloud_metadata, metadata)
-
-        return metadata
+        self._metadata = self._load_metadata()
