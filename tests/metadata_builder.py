60b0e76865b1d20f4d2ea1fbd1e2f467b8914109
Kyle Long <uilwen@gmail.com>
2016-04-11 12:54:08 -0500

    attempting to eliminate defining large datastructures

---
+++ tests/metadata_builder.py
@@ -0,0 +1,64 @@
+from pyshelf.metadata.keys import Keys
+from copy import deepcopy
+from pyshelf.resource_identity import ResourceIdentity
+from pyshelf.metadata.mapper import Mapper
+
+
+class MetadataBuilder(object):
+    DEFAULT = {
+        Keys.MD5: {
+            "immutable": True,
+            "name": Keys.MD5,
+            "value": Keys.MD5,
+        },
+        Keys.PATH: {
+            "immutable": True,
+            "name": Keys.PATH,
+            "value": Keys.PATH,
+        },
+        Keys.NAME: {
+            "immutable": True,
+            "name": Keys.NAME,
+            "value": Keys.NAME,
+        },
+    }
+
+    def __init__(self, data=None):
+        if not data:
+            data = MetadataBuilder.DEFAULT
+
+        self.data = deepcopy(data)
+
+    @property
+    def mapper(self):
+        if not self._mapper:
+            self._mapper = Mapper()
+
+        return self._mapper
+
+    def property(self, key, value, immutable=False):
+        """
+            Upserts a metadata value
+        """
+        self.data[key] = {
+            "immutable": immutable,
+            "name": key,
+            "value": value
+        }
+        return self
+
+    def version(self, value="1"):
+        return self.property("version", value)
+
+    def resource_url(self, resource_url):
+        identity = ResourceIdentity(resource_url)
+        self.identity = identity
+        self.data[Keys.PATH]["value"] = identity.artifact_path
+        self.data[Keys.NAME]["value"] = identity.artifact_name
+        return self
+
+    def to_cloud(self):
+        return self.mapper.to_cloud(self.data)
+
+    def copy(self):
+        return MetadataBuilder(self.data)
