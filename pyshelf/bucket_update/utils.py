2720652f4c10a57e9d0544b45aaaeaa1689be2bc
Kyle Long <uilwen@gmail.com>
2016-04-12 14:30:49 -0500

    specifically setting up elasticsearch instead

--- pyshelf/bucket_update/utils.py
+++ pyshelf/bucket_update/utils.py
@@ -16,11 +16,18 @@ def update_search_index(bucket_config):
 
 def _create_container(bucket_config):
     filename = os.path.join(bucket_config["bulkUpdateLogDirectory"], "{0}.log".format(bucket_config["name"]))
-    logging.basicConfig(
-        format="%(asctime)s:%(name)s:%(levelname)s:%(message)s",
-        filename=filename,
-        level=bucket_config["logLevel"],
-    )
-    logger = logging.getLogger(bucket_config["name"])
+    log_level = bucket_config["logLevel"]
+    _configure_logger("elasticsearch", filename, log_level)
+    logger = _configure_logger(bucket_config["name"], filename, log_level)
     container = Container(bucket_config, logger)
     return container
+
+
+def _configure_logger(name, filename, log_level):
+    logger = logging.getLogger(name)
+    handler = logging.FileHandler(filename)
+    formatter = logging.Formatter(fmt="%(asctime)s:%(name)s:%(levelname)s:%(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(log_level)
+    return logger
