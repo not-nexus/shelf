61eeeee4a76f690157a77b6495e1ce5bc59ab20c
Kyle Long <uilwen@gmail.com>
2016-04-15 13:45:32 -0500

    adding metadata
    Also making an important decision to NOT use realpath because it
    gets rid of trailing slashes which I don't want if I am dealing with
    a directory.

--- pyshelf/resource_identity.py
+++ pyshelf/resource_identity.py
@@ -7,8 +7,7 @@ class ResourceIdentity(object):
         if resource_url[0] != "/":
             resource_url = "/" + resource_url
 
-        # To get rid of redundant separators
-        self.resource_url = os.path.normpath(resource_url)
+        self.resource_url = resource_url
         self._bucket_name = None
         self._artifact_path = None
 
@@ -16,6 +15,7 @@ class ResourceIdentity(object):
         self._search = None
         self._cloud_metadata = None
         self._artifact_name = None
+        self._metadata = None
 
     @property
     def bucket_name(self):
@@ -66,6 +66,13 @@ class ResourceIdentity(object):
             self._cloud_metadata = os.path.join(self.artifact_path, "_metadata_{0}.yaml".format(self.artifact_name))
 
         return self._cloud_metadata
+
+    @property
+    def metadata(self):
+        if not self._metadata:
+            self._metadata = os.path.join(self.artifact_path, self.artifact_name, "_meta")
+
+        return self._metadata
 
     def _parse(self, resource_url):
         part_list = resource_url.split("/")
