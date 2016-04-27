ae54b2722423a08e8d94880c5a9c5860dadf1bfe
quantumew <AndyGertjejansen@outlook.com>
2016-04-27 12:24:47 -0500

    Remove old opt

--- bin/prune-search-index.py
+++ bin/prune-search-index.py
@@ -7,9 +7,6 @@ doc = """Usage: ./prune-search-index [options] <config-path>
     Options:
         -v --verbose                If set, the log level will be set to DEBUG.
 
-        -d=DIR --log-direcory=DIR   Directory to output logs to. Outputs to stdout
-                                    if directory not given.
-
     Arguments:
         <config-path>               Path to the yaml configuration file.
 """
