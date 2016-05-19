19a8b70ace056cdda0a7b3c4a8668babda08e40d
Kyle Long <uilwen@gmail.com>
2016-05-18 19:57:26 -0500

    adding old test

--- tests/cloud/storage_test.py
+++ tests/cloud/storage_test.py
@@ -8,17 +8,11 @@ class StorageTest(TestBase):
         logger = logging.Logger("StorageTest")
         self.storage = Storage("FakeAccessKey", "FakeSecretKey", "FakeBucketName", logger)
 
-    def test_travis_ci(self):
-        from datetime import datetime
-
+    def test_to_utc(self):
         date_string = "Wed, 18 May 2016 16:03:10 CST"
-        datetime.strptime(date_string, "%a, %d %b %Y %X")
-
-    #  def test_to_utc(self):
-    #      date_string = "Wed, 18 May 2016 16:03:10 CST"
-    #      expected_string = "2016-05-18T21:03:10Z"
-    #      new_date_string = self.storage._to_utc(date_string)
-    #      self.assertEqual(expected_string, new_date_string)
+        expected_string = "2016-05-18T21:03:10Z"
+        new_date_string = self.storage._to_utc(date_string)
+        self.assertEqual(expected_string, new_date_string)
 
     def test_to_utc_gmt(self):
         """
