edcb568796d47ca83a77f790d1aca1434002904d
Kyle Long <uilwen@gmail.com>
2016-09-22 14:12:30 -0500

    documenting

--- pyshelf/null_handler.py
+++ pyshelf/null_handler.py
@@ -2,5 +2,13 @@ import logging
 
 
 class NullHandler(logging.Handler):
+    """
+        A handler that does nothing.  Its if I want to make a logger
+        that doesn't log anything.
+    """
     def emit(self, record):
+        """
+            Args:
+                record(logging.LogRecord): Record to log.  I ignore it.
+        """
         pass
