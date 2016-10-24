751f97d5e778dec7a6af025cdeeb7c4df18beb0d
Kyle Long <uilwen@gmail.com>
2016-10-24 09:04:45 -0500

    implementing get_status

--- pyshelf/health.py
+++ pyshelf/health.py
@@ -1,3 +1,6 @@
+from pyshelf.health_status import HealthStatus
+
+
 class Health(object):
     """
         Responsible for storing and analyzing the health of the
@@ -49,3 +52,31 @@ class Health(object):
 
         # So that everything returns a list.
         return list(passing_set)
+
+    def get_status(self):
+        """
+            Determines the HealthStatus based off of some rules defined here.
+
+            1. Status is CRITICAL if search is not working or if we cannot
+            connect to 20% or more of the storages.
+            2. Status is WARNING if we cannot connect to less than 20% of buckets
+            but more than 0%.
+            3. Status is OK otherwise.
+        """
+        status = HealthStatus.OK
+        failing_list = self.get_failing_ref_name_list()
+        all_list = self._get_ref_name_list()
+
+        failing_count = len(failing_list)
+
+        percent_failing = float(failing_count) / float(len(all_list)) * 100
+
+        if percent_failing > 0:
+            if percent_failing < 20:
+                status = HealthStatus.WARNING
+            else:
+                status = HealthStatus.CRITICAL
+        elif not self.elasticsearch:
+            status = HealthStatus.CRITICAL
+
+        return status
--- tests/health_test.py
+++ tests/health_test.py
@@ -1,5 +1,6 @@
 from tests.test_base import TestBase
 from pyshelf.health import Health
+from pyshelf.health_status import HealthStatus
 
 
 class HealthTest(TestBase):
@@ -15,6 +16,12 @@ class HealthTest(TestBase):
                 },
                 {
                     "refName": "other"
+                },
+                {
+                    "refName": "four",
+                },
+                {
+                    "refName": "five"
                 }
             ]
         }
@@ -44,8 +51,61 @@ class HealthTest(TestBase):
 
         expected = [
             "hi",
-            "other"
+            "other",
+            "four",
+            "five"
         ]
 
         actual = self.health.get_passing_ref_name_list()
         self.assertEqual(sorted(expected), sorted(actual))
+
+    def run_get_status(self, elasticsearch, refNames, expected):
+        self.health.refNames = refNames
+        self.health.elasticsearch = elasticsearch
+        status = self.health.get_status()
+        self.assertEqual(expected, status)
+
+    def test_get_status_20_percent_failing_bucket(self):
+        self.run_get_status(
+            True,
+            {
+                "four": False,
+                "five": True,  # Just for fun, shouldn't break anything.
+            },
+            HealthStatus.CRITICAL
+        )
+
+    def test_get_status_less_than_20_percent_failing_bucket(self):
+        # I have to inflate the number of total buckets so that
+        # I can have more than 0% but less than 20% failing.
+        self.config["buckets"].append({
+            "refName": "six"
+        })
+
+        self.run_get_status(
+            True,
+            {
+                "four": False,
+            },
+            HealthStatus.WARNING
+        )
+
+    def test_get_status_passing(self):
+        self.run_get_status(
+            True,
+            {
+                "four": True,
+                "five": True,
+            },
+            HealthStatus.OK
+        )
+
+    def test_elasticsearch_unhealthy(self):
+        self.run_get_status(
+            False,
+            {
+                "four": True,
+                "five": True,
+            },
+            HealthStatus.CRITICAL
+        )
