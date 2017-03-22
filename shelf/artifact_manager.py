4c56fbda0b6166bb31247346ef53a65a1121f02f
Kyle Long <uilwen@gmail.com>
2017-03-22 13:37:19 -0500

    changing some debugs into infos

--- shelf/artifact_manager.py
+++ shelf/artifact_manager.py
@@ -46,7 +46,7 @@ class ArtifactManager(object):
             artifact_list = storage.get_directory_contents(directory_path, recursive=False)
 
             if artifact_list:
-                self.container.logger.debug("Resource {0} is assumed to be a directory.".format(directory_path))
+                self.container.logger.info("Resource {0} is assumed to be a directory.".format(directory_path))
                 artifact_path_list = [artifact.name for artifact in artifact_list]
                 self.link_manager.assign_listing(artifact_path_list)
             else:
